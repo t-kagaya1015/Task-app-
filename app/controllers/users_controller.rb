28911e4456df9ba5f45bf91de2c7ac2e1e6d5886
t-kagaya1015 <tnpp_kagaya1015@yahoo.co.jp>
2020-02-05 20:40:49 +0000

    master

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   end
   
   def index
-  @users = User.all
+   @users = User.paginate(page: params[:page])
   end 
 
   def new
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = '新規作成に成功しました。'
-      redirect_to @user
+      redirect_to(root_url) unless corrent_user?(@user)
     else
       render :new
     end
