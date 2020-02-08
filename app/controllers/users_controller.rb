d2a4f176a8e3b19407561942999c11f0f1969bf1
t-kagaya1015 <tnpp_kagaya1015@yahoo.co.jp>
2020-02-08 05:00:12 +0000

    index edit設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,13 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
-  def indx
-  end
- 
 
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -22,9 +18,22 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合の処理を記述します。
+    else
+      render :edit      
+    end
+  end
+
   private
-  
+
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
